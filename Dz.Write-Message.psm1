47a4700cf1955f1ef7e95717c2a714071928108b
DecimalZero <Chris.Muller@Neudesic.com>
2022-07-20 09:06:28 -0500

    Update Dz.Write-Message.psm1

--- Dz.Write-Message.psm1
+++ Dz.Write-Message.psm1
@@ -1,30 +1,18 @@
-# Get all scripts
-$Scripts = @(Get-ChildItem -Path $PSScriptRoot\Scripts\*.ps1 -ErrorAction SilentlyContinue -Recurse)
+# Region import everything we need
+$culture = $host.CurrentCulture.Name -replace '-\w*$', ''
+Import-LocalizedData  -UICulture $culture -BindingVariable Strings -FileName Strings -ErrorAction Ignore
 
-# Import all powershell files
-Foreach ($Script in @(Scripts)) {
-  Try {
-    . $Script.Fullname
-  }Catch {
-    Write-Error -Message "Failed to Import Function $($Script.Fullname): $_"
-  }
+If (-not $Strings) {
+  Import-LocalizedData  -UICulture "en" -BindingVariable Strings -FileName Strings -ErrorAction Ignore
 }
 
-# Export module member function
-Export-ModuleMember -Function 'Write-Message'
-
-# Get powershell manifest file path
-[String] $ManifestFile = '{0}.psd1' -f (Get-Item $PSCommandPath).BaseName;
-$ManifestFilePath = Join-Path -Path $PSScriptRoot -ChildPath $ManifestFile;
-
-# Test powershell manifest file path  
-If (Test-Path -Path $ManifestFilePath) {
-  # Check powershell manifest to powershell modules to remove
-  $Manifest = (Get-Content -Raw $ManifestFilePath) | Invoke-Expression;
-  Foreach ($ScriptToProcess in $Manifest.ScriptsToProcess) {
-    $ModuleToRemove = (Get-Item (Join-Path -Path $PSScriptRoot -ChildPath $ScriptToProcess)).BaseName;
-    If (Get-Module $ModuleToRemove) {
-      Remove-Module $ModuleToRemove;
-    }
-  }
+Try {
+  [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+}Catch {
+  Write-Warning -Message $Strings.SystemDrawingAvailable
 }
+
+Foreach ($Directory in @('Private', 'Public')) {
+  Get-ChildItem -Path "$PSScriptRoot\$Directory\*.ps1" | ForEach-Object {. $_.FullName}
+}
+
